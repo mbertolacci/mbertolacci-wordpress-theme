9a811c094dd761373dcfaae0dfa8aa263cf443e1
Michael Bertolacci <m.bertolacci@gmail.com>
2014-10-29 11:17:39 +1100

    Add post date to template

--- content.php
+++ content.php
@@ -3,6 +3,10 @@
         <a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
     </h1>
 
+    <div class="post-date">
+        <a class="url" href="<?= esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) ?>" rel="author"><?= get_the_author() ?></a> on <?= esc_html( get_the_date() ) ?>
+    </div>
+
     <div class="post-content">
         <?php the_content(); ?>
     </div>
